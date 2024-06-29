b758473c19350df529338cb631f06d25e1309219
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-06-30 00:22:47 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_HW = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = (hours_spent / completed_HW)
+print('Курс:', course_name,',', 'всего задач:', completed_HW,',', 'затрачено часов:', hours_spent,',','среднее время выполнения:', time_per_task)
